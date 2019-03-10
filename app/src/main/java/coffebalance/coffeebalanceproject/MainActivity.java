d19ed6da949d22506d13ec019b8f87d15d31adff
George <georgesims21@hotmail.com>
2019-03-10 13:52:56 +0100

    Added background image to main activity page, changed buttons. Unfinished.

--- app/src/main/java/coffebalance/coffeebalanceproject/MainActivity.java
+++ app/src/main/java/coffebalance/coffeebalanceproject/MainActivity.java
@@ -1,11 +1,15 @@
 package coffebalance.coffeebalanceproject;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,6 +19,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private VideoView videoBG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button loanBtn = findViewById(R.id.loanBtn);
         Button georgeBtn = findViewById(R.id.georgeBtn);
+
+        // (TODO) Add video background, 1440 x 2550p for samsung s7 -- @George
+        // Code for background video -- don't delete!
+//        videoBG = findViewById(R.id.videoBG);
+//        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.coffeecup);
+//        videoBG.setVideoURI(uri);
+//        videoBG.start();
+//
+//        videoBG.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+//            @Override
+//            public void onPrepared(MediaPlayer mp) {
+//                // Loop here
+//            }
+//        });
 
         loanBtn.setOnClickListener(new View.OnClickListener() {
             @Override
